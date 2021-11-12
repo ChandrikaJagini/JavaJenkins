5dcb095bb1c7459826d667f76a578e255e7907a9
ChandrikaJagini <91956379+ChandrikaJagini@users.noreply.github.com>
2021-11-12 15:40:34 +0530

    CodeCommit1.0

---
+++ JavaJenkinsClass.java
@@ -0,0 +1,19 @@
+
+
+
+	public class JavaJenkinsClass {
+
+		 
+
+	    public static void main(String[] args) {
+	        
+	        System.out.println("Hello Jenkins");
+
+	 
+
+	    }
+
+	 
+
+	}
+
